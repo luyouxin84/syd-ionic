0c1bb4af214de66e968eab5cea328fee13f17aa0
luyouxin84 <lu_youxin@hotmail.com>
2016-09-27 00:34:32 +0800

    26号晚上

--- app/pages/home/headbar.ts
+++ app/pages/home/headbar.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({	
 	selector:'head-bar',
 	templateUrl:'build/pages/home/headbar.html',
-	styleUrls:['/pages/home/headbar.css']	
+	
 })
 
 export class headBar {
 	
 	constructor() {
-		
+		console.log(location.pathname)
 	}
 }
