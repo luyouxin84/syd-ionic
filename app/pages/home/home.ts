2eb591762823ddf7f482943ccbeaaa5d43441583
luyouxin84 <lu_youxin@hotmail.com>
2016-09-28 17:54:24 +0800

    28号上班

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { headBar } from './headbar';
+import { mainTable } from './main-table-6';
+
 
 @Component({
   templateUrl: 'build/pages/home/home.html',
-  directives : [headBar]
+  directives : [headBar,mainTable]
 })
 export class HomePage {
   constructor(public navCtrl: NavController) {
-  
+
   }
 }
---
+++ app/pages/home/main-table-6.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Administrator on 2016/9/28.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl:'build/pages/home/main-table-6.html',
+  selector:'main-table'
+})
+export class mainTable{
+
+  constructor(){
+
+  }
+}
