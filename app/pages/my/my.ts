f97f3884225b026081d06bc4c0b17d8539b6b880
luyouxin84 <lu_youxin@hotmail.com>
2016-10-08 00:20:42 +0800

    1212

---
+++ app/pages/my/my.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by 卢友新 on 2016/10/8.
+ */
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl:'build/pages/my/my.html',
+
+})
+export class My{
+
+
+  constructor() {
+  }
+}
