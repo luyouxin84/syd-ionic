22c212616b1e86744366de11a862ed8b438afdb1
luyouxin84 <lu_youxin@hotmail.com>
2016-10-08 00:19:48 +0800

    7号晚上

--- app/pages/about/tableguide.ts
+++ app/pages/about/tableguide.ts
@@ -15,10 +15,16 @@ export class tableGuide{
   private cell_width:number;
   private items:Catalog[] = [];
   initlist() {
-    this.items.push({name : "12313", img :"my-icon1.png"})
-    this.items.push({name : "12313", img :"my-icon1.png"})
-    this.items.push({name : "12313", img :"my-icon1.png"})
-    this.items.push({name : "12313", img :"my-icon1.png"})
+    this.items.push({name : "跟团游 ", img :"my-icon1.png"})
+    this.items.push({name : "周边游", img :"my-icon6.png"})
+    this.items.push({name : "自助游", img :"my-icon5.png"})
+    this.items.push({name : "游轮", img :"my-icon4.png"})
+    this.items.push({name : "门票", img :"my-icon1.png"})
+    this.items.push({name : "自驾游", img :"my-icon6.png"})
+    this.items.push({name : "当地玩乐", img :"my-icon2.png"})
+    this.items.push({name : "机票", img :"my-icon5.png"})
+    this.items.push({name : "火车票", img :"my-icon1.png"})
+    this.items.push({name : "签证", img :"my-icon4.png"})
   }
   constructor() {
     let current_width = window.innerWidth;
